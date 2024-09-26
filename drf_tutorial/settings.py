47fff821c5a0b60efa622ea6c5dc75e042d0825e
Ifrah <ifrahshahid2020@gmail.com>
2024-09-27 01:49:29 +0500

    Add Snippet model serializer with basic serialization/deserialization tests

--- drf_tutorial/settings.py
+++ drf_tutorial/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'quickstart',
+    'snippets',
     'rest_framework',
 ]
 
